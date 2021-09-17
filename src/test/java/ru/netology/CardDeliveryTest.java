ab2f73a833336571228767939c75e9064056a317
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-09-17 12:42:49 +0300

    Edited yml, jar, Test.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -2,8 +2,11 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.exist;
@@ -12,18 +15,24 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+
+    String dateFormation(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void shouldTest() {
-//        LocalDate today = LocalDate.now();
-//        today.plusDays(4);
+        String dateMeeting = dateFormation(4);
+
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Санкт-Петербург");
-        $("[data-test-id='date'] [type='tel']").setValue("21.09.2021");
-        //$(withText("Дата встречи")).shouldBe(exist).setValue("21.09.2021");
+        $("[data-test-id='date'] [type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [type='tel']").setValue(dateMeeting);
         $("[name='name']").setValue("Елена");
-        $("[name='phone']").setValue("+79345678931");
+        $("[name='phone']").setValue("+79145678931");
         $("[data-test-id='agreement']").click();
-        $(byText("Запланировать")).click();
-        $(".notification__title").shouldHave(exactText("Успешно!"));
+        $(byText("Забронировать")).click();
+        $(".notification__title").shouldHave(exactText("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateMeeting));
     }
 }
